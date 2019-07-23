1f19f5c6ac467c6adc26d138a03fe56d9e0f077e
adaephonben <ma18btech11011@iith.ac.in>
2019-07-24 03:53:11 +0530

    Authentication + Delete API Call

--- main.go
+++ main.go
@@ -9,14 +9,17 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/gorilla/mux"
 	"net/http"
+	"golang.org/x/net/context"
+  firebase "firebase.google.com/go"
+  "google.golang.org/api/option"
 )
 
-type Booking struct {
-	Name string
-	RollNo string
+type BookingWithToken struct {
+	Email string
 	StartTime time.Time
 	EndTime time.Time
 	RouteID int
+	Token string
 }
 
 type UpdateQuery struct {
@@ -29,7 +32,7 @@ type UpdateResponse struct {
 	IsUpdateReqd bool
 }
 
-type Bookings []Booking 
+type Bookings []BookingWithToken
 
 const (
   host     = "localhost"
@@ -40,13 +43,14 @@ const (
 )
 
 var db *sql.DB
+var app *firebase.App
 
-func InsertBooking(booking Booking) {
+func InsertBooking(booking BookingWithToken) {
 	var err error
 	sqlStatement := `
-	INSERT INTO cab_sharing (Name, RollNo, StartTime, EndTime, RouteID)
+	INSERT INTO cab_sharing (Email, StartTime, EndTime, RouteID, UID)
 	VALUES ($1, $2, $3, $4, $5)`
-	_, err = db.Exec(sqlStatement, booking.Name, booking.RollNo, booking.StartTime, booking.EndTime, booking.RouteID)
+	_, err = db.Exec(sqlStatement, booking.Email, booking.StartTime, booking.EndTime, booking.RouteID, booking.Token)
 	if err != nil {
 		panic(err)
 	}
@@ -54,32 +58,49 @@ func InsertBooking(booking Booking) {
 
 func PublishHandler (w http.ResponseWriter, request *http.Request) {
 	decoder := json.NewDecoder(request.Body)
-	var booking Booking
+	var booking BookingWithToken
 	err := decoder.Decode(&booking)
 	if (err != nil) {
 		panic(err)
 	}
-	InsertBooking(booking)
+	client, err := app.Auth(context.Background())
+	if err != nil {
+		fmt.Printf("error getting Auth client: %v\n", err)
+		return
+	}
+	token, err := client.VerifyIDToken(request.Context(), booking.Token)
+	
+	if err != nil {
+		fmt.Printf("error verifying ID token: %v\n", err)
+		return
+	}
+	UID := token.UID
+	fmt.Printf("Verified ID token: %v\n", token)
+	var bookingWithUid = BookingWithToken{booking.Email, booking.StartTime, booking.EndTime, booking.RouteID, UID}
+	InsertBooking(bookingWithUid)
 }
 
 func QueryHandler (w http.ResponseWriter, request *http.Request) {
 	query := `select * from cab_sharing ;`
 	rows, err := db.Query(query)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return
 	}
 	defer rows.Close()	
 	var bookings Bookings
 	for rows.Next() {
-		var booking Booking 
-		if err := rows.Scan(&booking.Name, &booking.RollNo, &booking.StartTime, &booking.EndTime, &booking.RouteID); err != nil {
-			log.Fatal(err)
+		var booking BookingWithToken
+		if err := rows.Scan(&booking.Email, &booking.StartTime, &booking.EndTime, &booking.RouteID, &booking.Token); err != nil {
+			fmt.Println(err)
+			return
 		}
 		bookings = append(bookings, booking)
 	}
 	bookingsJSON, err := json.Marshal(bookings)
 	if err != nil {
-		log.Fatal("Cannot encode to JSON ", err)
+		fmt.Println("Cannot encode to JSON ", err)
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(bookingsJSON)
@@ -89,7 +110,8 @@ func isUpdateRequired(QueryTimeStart time.Time, QueryTimeEnd time.Time, RouteID 
 	query := `select * from cab_sharing where StartTime <= $1 and EndTime >= $2 and RouteID = $3;`
 	rows, err := db.Query(query, QueryTimeEnd, QueryTimeStart, RouteID)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return false
 	}
 	defer rows.Close()	
 	doesExist := false
@@ -116,6 +138,35 @@ func UpdateHandler(w http.ResponseWriter, request *http.Request) {
 	w.Write(updateJSON)
 }
 
+func DeleteHandler(w http.ResponseWriter, request *http.Request) {
+	decoder := json.NewDecoder(request.Body)
+	var booking BookingWithToken
+	err := decoder.Decode(&booking)
+	if (err != nil) {
+		panic(err)
+	}
+	client, err := app.Auth(context.Background())
+	if err != nil {
+		fmt.Printf("error getting Auth client: %v\n", err)
+		return
+	}
+	token, err := client.VerifyIDToken(request.Context(), booking.Token)
+	
+	if err != nil {
+		fmt.Printf("error verifying ID token: %v\n", err)
+		return
+	}
+	UID := token.UID
+	fmt.Printf("Verified ID token: %v\n", token)
+	sqlStatement := `
+	DELETE FROM cab_sharing
+	WHERE Email = $1 AND StartTime = $2 AND EndTime = $3 AND RouteID = $4 AND UID = $5;`
+	_, err = db.Exec(sqlStatement, booking.Email, booking.StartTime, booking.EndTime, booking.RouteID, UID)
+	if err != nil {
+		fmt.Println("Error deleting record ", err)
+	}
+}
+
 func main() {
 	var err error 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
@@ -131,11 +182,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	opt := option.WithCredentialsFile("aims-helper-247320-fd071f06e365.json")
+	app, _ = firebase.NewApp(context.Background(), nil, opt)
 	fmt.Println("Successfully connected to PostgreSQL Database!")
 	r := mux.NewRouter()
 	r.HandleFunc("/publish", PublishHandler)
 	r.HandleFunc("/query", QueryHandler)
 	r.HandleFunc("/update", UpdateHandler)
+	r.HandleFunc("/delete", DeleteHandler)
 	fmt.Println("Set up server.")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
